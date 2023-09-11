8fe23c9464e21dd4a4d05b242a6463f934dd27e9
QASdzxcaqw <alexzed01@bk.ru>
2023-09-11 14:58:41 +0300

    First

---
+++ Task1/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cmath>
+
+// Определяем нашу функцию f(x)
+double f(double x) {
+    return x*x*x-3;
+    // Здесь опишите вашу функцию
+    // Пример: return x*x - 4; для уравнения x^2 = 4
+}
+
+// Метод половинного деления для отделения корней
+double bisection(double A, double B, double epsilon) {
+    double a = A;
+    double b = B;
+    double c;
+
+    if (f(a) * f(b) >= 0) {
+        std::cout << "Не удовлетворяет условию: f(a) * f(b) должно быть < 0" << std::endl;
+        return 0.0; // Возвращаем ноль, чтобы показать ошибку
+    }
+
+    while ((b - a) >= epsilon) {
+        c = (a + b) / 2;
+        if (f(c) == 0.0) {
+            return c; // Найден корень
+        } else if (f(c) * f(a) < 0) {
+            b = c;
+        } else {
+            a = c;
+        }
+    }
+    return (a + b) / 2;
+}
+
+// Метод Ньютона для уточнения корней
+double newton(double x0, double epsilon, int max_iterations) {
+    double x = x0;
+    for (int i = 0; i < max_iterations; i++) {
+        double fx = f(x);
+        double fpx = (f(x + epsilon) - fx) / epsilon;
+        x = x - fx / fpx;
+        if (std::abs(f(x)) < epsilon) {
+            return x; // Найден корень
+        }
+    }
+    return 0.0; // Возвращаем ноль, чтобы показать ошибку
+}
+
+int main() {
+    double A, B;
+    double epsilon = 0.0001; // Точность
+    int max_iterations = 100; // Максимальное количество итераций для метода Ньютона
+
+    // Задаем интервал [A, B]
+    A = -5; // Задайте начало интервала
+    B = 10; // Задайте конец интервала
+
+    // Отделение корней
+    double root = bisection(A, B, epsilon);
+
+    if (root != 0.0) {
+        // Уточнение корней методом Ньютона
+        double refined_root = newton(root, epsilon, max_iterations);
+        std::cout << "Корень уточненный: " << refined_root << std::endl;
+
+        // Дополнительный контроль и анализ результатов, если необходимо
+        // ...
+
+    } else {
+        std::cout << "Корень не найден на заданном интервале." << std::endl;
+    }
+
+    return 0;
+}
