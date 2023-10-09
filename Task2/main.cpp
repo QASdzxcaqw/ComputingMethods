92cb380c38ef94288724d23dd3ebe342ca2e711d
QASdzxcaqw <alexzed01@bk.ru>
2023-10-09 13:41:14 +0300

    fix: fill and sort feat: implement newton interpolation

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -2,34 +2,28 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <iomanip>
 int m, n;
 double a, b, x0;
 
 
 double f(double x){
-    return cos(x) + 2*x;
+    return log(x);
 }
 
 bool compareByDifference( const std::pair<double, double>& target1, const std::pair<double, double>& target2) {
     return std::abs(target1.first - x0) < std::abs(target2.first - x0);
 }
 
-void fillTable(std::vector<double>& table_x, std::vector<double>& table_y){
-    for (int i = 0; i <= m; i++) {
-        table_x[i] = a + i * (b - a) / m;
-        table_y[i] = f(a + i * (b - a) / m);
-    }
-}
 
-void sortTableByProximity(std::vector<std::pair<double, double>>& sorted_table,  std::vector<double>& table_x,  std::vector<double>& table_y) {
-    for (int i = 0; i <= n; i++) {
-        sorted_table[i].first = table_x[i];
-        sorted_table[i].second = table_y[i];
+void fillSortTableByProximity(std::vector<std::pair<double, double>>& sorted_table) {
+    for (int i = 0; i <= m; i++) {
+        sorted_table[i].first = a + i * (b - a) / m;
+        sorted_table[i].second = f(a + i * (b - a) / m);
     }
     std::sort(sorted_table.begin(), sorted_table.end(), [&](const std::pair<double, double>& target1, const std::pair<double, double>& target2) {
         return compareByDifference(target1, target2);
     });
-
     sorted_table.resize(n+1);
 
     std::cout << "Отсортированная таблица (или набор узлов, ближайших к точке x):" << std::endl;
@@ -53,18 +47,49 @@ double lagrangeInterpolation(std::vector<std::pair<double, double>>& sorted_tabl
     return result;
 }
 
+double newton_interpolation(std::vector<std::pair<double, double>>& sorted_table) {
+    double result = 0.0;
+
+    // Вычисление разделенных разностей
+    std::vector<std::vector<double>> divided_diff(n+1, std::vector<double>(n+1, 0.0));
+    for (int i = 0; i <= n; i++) {
+        divided_diff[i][0] = sorted_table[i].second;
+    }
+
+    for (int j = 1; j <= n; j++) {
+        for (int i = 0; i <= n - j; i++) {
+            divided_diff[i][j] = (divided_diff[i + 1][j - 1] - divided_diff[i][j - 1]) / (sorted_table[i + j].first - sorted_table[i].first);
+        }
+    }
+//    std::cout<<"Таблица разностей для метода Ньютона:"<<std::endl;
+//    for (auto& row :divided_diff){
+//        for(auto& cell: row){
+//            std::cout <<cell << "\t";
+//        }
+//        std::cout<<std::endl;
+//    }
+
+    // Вычисление интерполяционного значения
+    for (int i = 0; i < n; i++) {
+        double term = divided_diff[0][i];
+        for (int j = 0; j < i; j++) {
+            term *= (x0 - sorted_table[j].first);
+        }
+        result += term;
+    }
+
+    return result;
+}
+
 int main() {
     std::cout << "Задача алгебраического интерполирования" << std::endl;
 
     // Ввод параметров задачи
-
     std::cout << "Введите число значений в таблице (m+1): ";
     std::cin >> m;
     std::cout << "Введите концы отрезка [a, b]: ";
     std::cin >> a >> b;
 
-    std::vector<double> table_x(m + 1);
-    std::vector<double> table_y(m + 1);
     std::vector<std::pair<double, double>> sorted_table(m+1);
 
     // Ввод точки интерполирования x
@@ -80,13 +105,19 @@ int main() {
         return 1;
     }
 
-    fillTable(table_x, table_y);
-    sortTableByProximity(sorted_table, table_x, table_y);
+    fillSortTableByProximity(sorted_table);
 
     // Вывод результатов
-
     double lagrangeResult = lagrangeInterpolation(sorted_table);
-    std::cout<<"Результат в форме лагранжа: "<<lagrangeResult<<std::endl;
+    double newtonResult = newton_interpolation(sorted_table);
+    double actualErrorLagrange = std::abs(lagrangeResult - f(x0));
+    double actualErrorNewton = std::abs(newtonResult - f(x0));
 
+    std::cout << std::fixed;
+    std::cout << std::setprecision(10);
+    std::cout<<"Результат в форме лагранжа: "<<lagrangeResult<<std::endl;
+    std::cout<<"Результат в форме ньютона: "<<newtonResult<<std::endl;
+    std::cout << "Значение абсолютной фактической погрешности для формы Лагранжа: " << actualErrorLagrange << std::endl;
+    std::cout << "Значение абсолютной фактической погрешности для формы Ньютона: " << actualErrorNewton << std::endl;
     return 0;
 }
