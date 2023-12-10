a9f82a5932de25e0aba26cef779969b31aa2ee86
QASdzxcaqw <alexzed01@bk.ru>
2023-12-11 02:56:19 +0300

    feat: runge, theory error

--- Task4/script.js
+++ Task4/script.js
@@ -3,11 +3,51 @@
 //3x^2+2x+2
 // }
 
-function integral(k){
-    return k * k * k + 2 * k * k + 7 * k;
+// Функция для вычисления значения производной порядка n методом конечных разностей
+function nthDerivative(f, x, h, n) {
+    if (n === 0) {
+        return f(x);
+    }
+
+    let result = 0;
+    for (let i = 0; i <= n; i++) {
+        result += Math.pow(-1, i) * binomialCoefficient(n, i) * f(x + (n - 2 * i) * h);
+    }
+
+    return result / Math.pow(h, n);
 }
 
-function exactIntegral(a, b) {
+// Функция для вычисления биномиального коэффициента C(n, k)
+function binomialCoefficient(n, k) {
+    if (k === 0 || k === n) {
+        return 1;
+    }
+
+    let result = 1;
+    for (let i = 1; i <= k; i++) {
+        result *= (n - i + 1) / i;
+    }
+
+    return Math.round(result);
+}
+
+// Функция для нахождения максимального значения производной порядка n на заданном отрезке
+function findMaxDerivativeOnInterval(f, a, b, order, h = 0.002) {
+
+    let maxDerivative = Number.NEGATIVE_INFINITY;
+
+    for (let x = a; x <= b; x += h) {
+        const currentDerivative = nthDerivative(f, x, h, order);
+        
+        if (currentDerivative > maxDerivative) {
+            maxDerivative = currentDerivative;
+        }
+    }
+
+    return maxDerivative;
+}
+
+function exactIntegral(a, b, integral) {
     return (integral(b) - integral(a));
 }
 
@@ -50,17 +90,40 @@ function threeEighths(xi, h, f) {
 
 function runge(val1, val2, d, l){
     //left right 0, mid trap = 1, simps = 3
-    let mult = (l^(d+1)* val1- val2)/(l^(d+1)-1);
+    let mult = (l ** (d+1) * val1- val2)/(l ** (d+1)-1);
     return mult;
 }
+
+function fillErrorRate(arr, it){
+    for(let j = 0; j < 6; j++){
+        arr[it].push(Math.abs(arr[it-1][j + 2] - arr[0][1]));
+    }
+}
+
 function fillArray(a, b, n, func, arr){
     
     let quadFuncs = [leftRectangle, rightRectangle, midRectangle, trapezoid, simpson, threeEighths]
     for(let i = 0; i < quadFuncs.length; i++){
         arr[0].push(integrateUsingQuadrature(a, b, n, quadFuncs[i], func));
     }
-    for(let j = 0; j < quadFuncs.length; j++){
-        arr[1].push(Math.abs(arr[0][j + 2] - arr[0][1]));
+    
+}
+
+function fillRunge(arr1, arr2){
+    for(let i = 2; i < 8; i++){
+        var ast = [0, 0, 1, 1, 3, 0];
+        arr1[2].push(runge(arr1[0][i], arr2[0][i], ast[i-2], l));
+    }
+    
+    fillErrorRate(arr1, 3);
+
+}
+function fillTheory(arr, a, b, m, f){
+    let h = (b-a)/m;
+    const cAst = [1/2, 1/2, 1/24, 1/12, 1/2880];
+    var ast = [1, 1, 2, 1, 1];
+    for(let i = 2; i < cAst.length+2; i++){
+        arr[2].push(cAst[i-2]*findMaxDerivativeOnInterval(f, a, b, ast[i-2])*(b-a)*(h ** (ast[i-2])));
     }
 }
 
@@ -71,10 +134,8 @@ document.getElementById('createTableBtn').addEventListener('click', function () 
     var userInput = document.getElementById('func').value;
     var func = new Function('x', 'return ' + userInput);
 
-    // const f = '2 * x^2 + 3 * x';
-    // const res = math.evaluate('integral(${fu}, x, 0, 1)');
-
-    //var integ = new Function('x', 'return ' + expr);
+    var userIntegral = document.getElementById('integral').value;
+    var integral = new Function('x', 'return ' + userIntegral);
 
 
     const a = parseFloat(document.getElementById('lowerLimit').value);
@@ -86,7 +147,14 @@ document.getElementById('createTableBtn').addEventListener('click', function () 
         return;
     }
 
-    const exactValue = exactIntegral(a, b);
+    const exactValue = exactIntegral(a, b, integral);
+
+    let rows = 2;
+    let colums = 7;
+    let dev = false;
+    let theory = false;
+
+    var rowsval = ['Точное значение', 'Погрешность'];
 
     if(isNaN(n) || l <= 0){
         n = 1;
@@ -94,30 +162,53 @@ document.getElementById('createTableBtn').addEventListener('click', function () 
     }
     else if(isNaN(l)){
         l = 1;
+        theory = true;
+        rows = 3;
+        rowsval.push('Теоретическая погрешность');
+    
     }else{
+        dev = true;
+        rows = 4;
         let compose = n*l;
+        var composeVal = [[compose, exactValue], ['', 0], ['', ''], ['', 0]];
+        fillArray(a, b, compose, func, composeVal);
+        fillErrorRate(composeVal, 1);
+        rowsval.push('Уточненные значения по Рунге');
+        rowsval.push('Погрешность Рунге');
     }
-    let colums = 7
+    
     if(n == 1){
         colums = 8;
     }
 
     var valtest = [[n, exactValue], ['', 0]];
     fillArray(a, b, n, func, valtest);
+    fillErrorRate(valtest, 1);
+
+    if(theory){
+        valtest.push(['', 0]);
+        fillTheory(valtest, a, b, n, func);
+    }
+
+    if(dev){
+        fillRunge(composeVal, valtest);
+        valtest = composeVal;
+    }
+   
+    
     
     // Создаем элемент таблицы
     var table = document.createElement('table');
     table.classList.add('table');
     
-    var rows = ['Точное значение', 'Погрешность']
     
     var tbody = document.createElement('tbody');
-    for (var i = 1; i <= 2; i++) {
+    for (var i = 1; i <= rows; i++) {
         var row = document.createElement('tr');
         
         var thRow = document.createElement('th');
         thRow.scope = 'row';
-        thRow.textContent = rows[i-1];
+        thRow.textContent = rowsval[i-1];
         row.appendChild(thRow);
 
         for (var j = 1; j <= colums; j++) {
